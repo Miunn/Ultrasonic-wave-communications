fff6d912227bbb75420427fc07c5ee652b9771eb
Miunn <remcaulier@orange.fr>
2024-10-05 10:58:04 +0200

    Fix merging

--- c/src/can/can.c
+++ c/src/can/can.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "can/can.h"
 
 int createCanMessage(int can_id, char *data, int data_len, char *_frame) {
@@ -9,15 +8,3 @@ int decodeCanMessage(char *frame_buffer, int f_buff_len, char *_data,
                      int *_data_len, int *_can_id) {
   return 1;
 }
-=======
-#include "can/can.h"
-
-int createCanMessage(int can_id, char *data, int data_len, char *_frame) {
-  return -1;
-}
-
-int decodeCanMessage(char *frame_buffer, int f_buff_len, char *_data,
-                     int *_data_len, int *_can_id) {
-  return 1;
-}
->>>>>>> signal_processing_recover
