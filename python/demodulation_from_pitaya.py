f316fcbe09580785eb535b90d4885c2f639bd9ff
Rémi Caulier <Miunn>
2024-11-13 16:20:46 +0100

    Decision config

--- python/demodulation_from_pitaya.py
+++ python/demodulation_from_pitaya.py
@@ -6,54 +6,80 @@ import matplotlib.pyplot as plot
 import struct
 from signal_processing.psk_modulation import bpsk_demodulation, butter_lowpass_filter, decision
 
-IP = '10.42.0.125'
+class Read_Pitaya:
+    IP = '169.254.67.34'
+    DEC = 1
+    TRIG_LVL = 0.6
 
-rp_s = scpi.scpi(IP)
+    def __init__(self, ip='169.254.67.34', dec=1, trig_lvl=0.6):
+        self.IP = ip
+        self.DEC = dec
+        self.TRIG_LVL = trig_lvl
 
-rp_s.tx_txt('ACQ:RST')
+        self.rp_s = scpi.scpi(self.IP)
 
-dec = 1
-trig_lvl = 0.4
+    def close(self):
+        self.rp_s.close()
 
-# Function for configuring Acquisition
-rp_s.acq_set(dec, trig_lvl, units='volts', sample_format='bin', trig_delay=8000)
+    def tx_txt(self, txt):
+        self.rp_s.tx_txt(txt)
 
-rp_s.tx_txt('ACQ:START')
-rp_s.tx_txt('ACQ:TRig CH1_PE')
+    def rx_txt(self):
+        return self.rp_s.rx_txt()
+    
+    def acq_set(self, dec, trig_lvl, units='volts', sample_format='bin', trig_delay=8000):
+        self.rp_s.acq_set(dec, trig_lvl, units=units, sample_format=sample_format, trig_delay=trig_delay)
 
-while 1:
-    rp_s.tx_txt('ACQ:TRig:STAT?')
-    if rp_s.rx_txt() == 'TD':
-        break
+    def acq_data(self, channel, binary=True, convert=True):
+        return self.rp_s.acq_data(channel, binary=binary, convert=convert)
 
-## ! OS 2.00 or higher only ! ##
-while 1:
-    rp_s.tx_txt('ACQ:TRig:FILL?')
-    if rp_s.rx_txt() == '1':
-        break
+    def read(self, out='signal.bin'):
+        self.tx_txt('ACQ:RST')
+        self.acq_set(self.DEC, self.TRIG_LVL, units='volts', sample_format='bin', trig_delay=8000)
+        self.tx_txt('ACQ:START')
+        self.tx_txt('ACQ:TRig CH1_PE')
+
+        while 1:
+            self.tx_txt('ACQ:TRig:STAT?')
+            if self.rx_txt() == 'TD':
+                break
+
+        while 1:
+            self.tx_txt('ACQ:TRig:FILL?')
+            if self.rx_txt() == '1':
+                break
+
+        buff = self.acq_data(1, binary=True, convert=True)
+
+        with open(out, 'w') as f:
+            f.write(' '.join([str(f) for f in buff]))
+
+        return buff
 
 
-# function for Data Acquisition
-buff = rp_s.acq_data(1, binary=True, convert=True)
 
-#with open('pitayareadings-burst.bin', 'w') as f:
-#    f.write(' '.join([str(f) for f in buff]))
 
-print(buff)
-import numpy as np
-plot.plot(buff)
-
-demodulated = bpsk_demodulation(buff, freq=5)
-plot.plot(demodulated)
-
-lpf = butter_lowpass_filter(demodulated, 3, 1000, order=3)
-
-bits = decision(lpf)
-print(''.join([str(i) for i in bits]))
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        with open(sys.argv[1], 'r') as f:
+            buff = [float(s) for s in f.readline().split()]
         
-plot.plot(lpf)
+        data_lpf = butter_lowpass_filter(buff, 2, 1000, order=3)
+        print("Max data_lpf:", max(data_lpf))
 
-rp_s.close()
+        plot.plot(buff)
+        plot.plot(data_lpf)
+    else:
+        rp = Read_Pitaya(ip='10.42.0.125', dec=16)
+        signal_acquired = False
+        n = 0
+        buff = rp.read(out='signal-dec-16-ok.bin')
+            
+        data_lpf = butter_lowpass_filter(buff, 2, 1000, order=3)
 
-plot.ylabel('Voltage')
-plot.show()
+        plot.plot(buff)
+        plot.plot(data_lpf)
+        plot.ylabel('Voltage')
+        plot.title('Signal')
+
+    plot.show()
--- python/modulation_to_pitaya.py
+++ python/modulation_to_pitaya.py
@@ -9,19 +9,29 @@ IP = '10.42.0.125'
 rp_s = scpi.scpi(IP)
 
 if len(sys.argv) >= 2:
+    if sys.argv[1] == 'stop':
+        rp_s.tx_txt('OUTPUT1:STATE OFF')
+        rp_s.close()
+        sys.exit(0)
+
+    if sys.argv[1] != 'sine':
         bits = [int(i) for i in sys.argv[1]]
         print(len(bits))
         mod = psk_modulation(bits, freq=5)
         print(len(mod))
+        plt.plot(mod)
+
 else:
     print("Provide a bit sequence as argument")
     sys.exit(1)
 
-plt.plot(mod)
 
 rp_s.tx_txt('GEN:RST')
 
-rp_s.sour_set(1, 'arbitrary', 1, 300000/(len(bits)*5), data=mod, burst=True)
+if sys.argv[1] == 'sine':
+    rp_s.sour_set(1, 'sine', 1, 300000, nor=2)
+else:
+    rp_s.sour_set(1, 'arbitrary', 1, 300000/(len(bits)*5), data=mod, burst=True)
 
 rp_s.tx_txt('OUTPUT1:STATE ON')
 rp_s.tx_txt('SOUR1:TRig:INT')
@@ -29,7 +39,7 @@ rp_s.tx_txt('SOUR1:TRig:INT')
 
 rp_s.close()
 
-plt.show()
+#plt.show()
 
 
 """
